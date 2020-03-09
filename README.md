8250650693dcac09cfa1ced88c43fb3c1f990bca
ChurakovMike <churakovmike@mail.ru>
2020-03-09 13:41:10 +0700

    + add files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Yii2-free-kassa
 Yii2 free kassa extension
+
+secret1 = zn8rearq
+secret2 = vjo7wsdb
+merchantId = 194851
---
+++ src/FreeKassaComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ChurakovMike\Freekassa;
+
+use yii\base\Component;
+
+/**
+ * Компонент для работы с free-kassa
+ *
+ * Class FreeKassaComponent.
+ * @package ChurakovMike\Freekassa
+ */
+class FreeKassaComponent extends Component
+{
+
+}
